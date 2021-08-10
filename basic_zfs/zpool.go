bcb34832460e5d5be981e199a0f4e41b350780b7
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2021-08-10 11:57:03 +0200

    feat(basic_zpool): implement fetching zpools with properties

--- basic_zfs/zpool.go
+++ basic_zfs/zpool.go
@@ -14,3 +14,24 @@ func ListPools() ([]string, error) {
 	output := string(out[:])
 	return strings.Fields(output), nil
 }
+
+func PoolProperties(pools []string, poolProperties []string) ([][]string, error) {
+	joinedDsProps := strings.Join(poolProperties, ",")
+	zpoolArgs := []string{"list", "-Hpo", joinedDsProps}
+	zpoolArgs = append(zpoolArgs, pools...)
+	outBytes, err := exec.Command("zpool", zpoolArgs...).Output()
+	if err != nil {
+		return nil, err
+	}
+
+	output := string(outBytes)
+	lines := strings.Split(output, "\n")
+	results := make([][]string, 0, len(lines))
+	for _, line := range lines {
+		// NOTE: last line is empty
+		if len(line) > 0 {
+			results = append(results, strings.Fields(line))
+		}
+	}
+	return results, nil
+}
