pdf/zfs_exporter

commit 850c4359beb7dc1492b881daa1831d507832bd10

feat(collector): collect keystatus property

collector/transform.go
@@ -19,6 +19,14 @@ const (
 	poolSuspended
 )
 
+type keystatusCode int
+
+const (
+	keystatusNone keystatusCode = iota
+	keystatusAvailable
+	keystatusUnavailable
+)
+
 func transformNumeric(value string) (float64, error) {
 	if value == `-` || value == `none` {
 		return 0, nil
@@ -45,6 +53,22 @@ func transformHealthCode(status string) (float64, error) {
 		result = poolSuspended
 	default:
 		return -1, fmt.Errorf(`unknown pool heath status: %s`, status)
+	}
+
+	return float64(result), nil
+}
+
+func transformKeystatus(status string) (float64, error) {
+	var result keystatusCode
+	switch zfs.Keystatus(status) {
+	case zfs.KeystatusNone:
+		result = keystatusNone
+	case zfs.KeystatusAvailable:
+		result = keystatusAvailable
+	case zfs.KeystatusUnavailable:
+		result = keystatusUnavailable
+	default:
+		return -1, fmt.Errorf(`unknown keystatus: %s`, status)
 	}
 
 	return float64(result), nil

collector/dataset.go
@@ -181,6 +181,18 @@ var (
 				prometheus.GaugeValue,
 				datasetLabels...,
 			),
+			`keystatus`: newProperty(
+				subsystemDataset,
+				`keystatus`,
+				fmt.Sprintf("Indicates if the encryption key is currently loaded [%d: %s, %d: %s, %d: %s].",
+					keystatusNone, zfs.KeystatusNone,
+					keystatusAvailable, zfs.KeystatusAvailable,
+					keystatusUnavailable, zfs.KeystatusUnavailable,
+				),
+				transformKeystatus,
+				prometheus.GaugeValue,
+				datasetLabels...,
+			),
 		},
 	}
 )

collector/dataset_test.go
@@ -30,7 +30,7 @@ func TestDatsetMetrics(t *testing.T) {
 			name:           `all metrics`,
 			kinds:          []zfs.DatasetKind{zfs.DatasetFilesystem},
 			pools:          []string{`testpool`},
-			propsRequested: []string{`available`, `compressratio`, `logicalused`, `logicalreferenced`, `quota`, `refcompressratio`, `referenced`, `refquota`, `refreservation`, `reservation`, `snapshot_count`, `snapshot_limit`, `used`, `usedbychildren`, `usedbydataset`, `usedbyrefreservation`, `usedbysnapshots`, `volsize`, `written`, `creation`},
+			propsRequested: []string{`available`, `compressratio`, `logicalused`, `logicalreferenced`, `quota`, `refcompressratio`, `referenced`, `refquota`, `refreservation`, `reservation`, `snapshot_count`, `snapshot_limit`, `used`, `usedbychildren`, `usedbydataset`, `usedbyrefreservation`, `usedbysnapshots`, `volsize`, `written`, `creation`, `keystatus`},
 			metricNames:    []string{`zfs_dataset_available_bytes`, `zfs_dataset_compression_ratio`, `zfs_dataset_logical_used_bytes`, `zfs_dataset_logical_referenced_bytes`, `zfs_dataset_quota_bytes`, `zfs_dataset_referenced_compression_ratio`, `zfs_dataset_referenced_bytes`, `zfs_dataset_referenced_quota_bytes`, `zfs_dataset_reservation_bytes`, `zfs_dataset_snapshot_count_total`, `zfs_datset_snapshot_limit_total`, `zfs_dataset_used_bytes`, `zfs_dataset_used_by_children_bytes`, `zfs_dataset_used_by_datset_bytes`, `zfs_datset_used_by_referenced_reservation_bytes`, `zfs_dataset_used_by_snapshot_bytes`, `zfs_dataset_volume_size_bytes`, `zfs_dataset_written_bytes`},
 			propsResults: map[string][]datasetResults{
 				`testpool`: {
@@ -56,6 +56,7 @@ func TestDatsetMetrics(t *testing.T) {
 							`volsize`:              `1024`,
 							`written`:              `1024`,
 							`creation`:             `1756033110`,
+							`keystatus`:            `available`,
 						},
 					},
 				},
@@ -105,6 +106,9 @@ zfs_dataset_written_bytes{name="testpool/test",pool="testpool",type="filesystem"
 # HELP zfs_dataset_creation_timestamp The unix timestamp when this dataset was created.
 # TYPE zfs_dataset_creation_timestamp gauge
 zfs_dataset_creation_timestamp{name="testpool/test",pool="testpool",type="filesystem"} 1756033110
+# HELP zfs_dataset_keystatus Indicates if the encryption key is currently loaded [0: none, 1: available, 2: unavailable]
+# TYPE zfs_dataset_keystatus gauge
+zfs_dataset_keystatus{name="testpool/test",pool="testpool",type="filesystem"} 1
 `,
 		},
 		{

zfs/dataset.go
@@ -16,6 +16,15 @@ const (
 	DatasetSnapshot DatasetKind = `snapshot`
 )
 
+// Keystatus enum contains keystatus string value
+type Keystatus string
+
+const (
+	KeystatusNone        Keystatus = `none`
+	KeystatusAvailable   Keystatus = `available`
+	KeystatusUnavailable Keystatus = `unavailable`
+)
+
 type datasetsImpl struct {
 	pool string
 	kind DatasetKind

commit c96a9578e54cb74d32d541375dd361652df188c9

fix(collector): handle keystatus=-
turns out that even though `keystatus` is documented to have the value
`none` for unencrypted datasets, `zfs list` and `zfs get` display it as
`-`.

fixes the following error when collecting keystatus:

	{"time":"2026-08-10T13:18:46.191309066Z","level":"ERROR","source":"zfs.go:217","msg":"Executing collector","status":"error","collector":"dataset-filesystem","durationSeconds":0.014250391,"err":"unknown keystatus: -"}

Fixes: 850c4359beb7dc1492b881daa1831d507832bd10

collector/transform.go
@@ -61,6 +61,8 @@ func transformHealthCode(status string) (float64, error) {
 func transformKeystatus(status string) (float64, error) {
 	var result keystatusCode
 	switch zfs.Keystatus(status) {
+	case `-`:
+		fallthrough
 	case zfs.KeystatusNone:
 		result = keystatusNone
 	case zfs.KeystatusAvailable: